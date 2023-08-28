4ab687e19471c7e55e5eb28b9caa2aa16fe1cba6
Khalil Masree <33801628+khalilmasri@users.noreply.github.com>
2023-08-28 20:17:26 +0300

    Enhance: Added comments and docstring for each function

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,4 +1,13 @@
 def calculate_total(cart):
+    """
+    Calculate the total price of items in the cart.
+    
+    Parameters:
+    cart (list): List of dictionaries representing items in the cart.
+    
+    Returns:
+    float: Total price of all items.
+    """
     total = 0
     for item in cart:
         if 'price' in item and isinstance(item['price'], (int, float)):
@@ -13,15 +22,14 @@ def display_total(total):
     else:
         print("Invalid total price. Cart may be empty!")
 
-CART = [
+cart = [
     {'name': 'Item A', 'price': 10.99},
     {'name': 'Item B', 'price': 5.99},
     {'name': 'Item C', 'price': 8.49}
 ]
 
-for item in CART:
+for item in cart:
     print(f"Item: {item['name']} - Price: ${item['price']}")
 
-shopping_cart_total = calculate_total(CART)
+shopping_cart_total = calculate_total(cart)
 display_total(shopping_cart_total)
-
